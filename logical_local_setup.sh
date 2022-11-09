21a0dab9c45d3a89b53a18175b7fb4179e3e41db
Melanie Plageman <melanieplageman@gmail.com>
2022-11-09 09:11:20 -0500

    set timeouts

--- logical_local_setup.sh
+++ logical_local_setup.sh
@@ -53,22 +53,32 @@ PSQL_REPLICA=("$INSTALL_DIR"/psql -p "$REPLICA_PORT" -d "$DB")
 "${PSQL_PRIMARY[@]}" -c "ALTER SYSTEM SET log_min_messages = DEBUG2;"
 "${PSQL_REPLICA[@]}" -c "ALTER SYSTEM SET log_min_messages = DEBUG2;"
 
+# Set wal sender and receiver timeouts
+"${PSQL_REPLICA[@]}" -c "ALTER SYSTEM SET wal_receiver_timeout='3000s'"
+"${PSQL_PRIMARY[@]}" -c "ALTER SYSTEM SET wal_receiver_timeout='3000s'"
+"${PSQL_PRIMARY[@]}" -c "ALTER SYSTEM SET wal_sender_timeout='3000s'"
+"${PSQL_REPLICA[@]}" -c "ALTER SYSTEM SET wal_sender_timeout='3000s'"
+
 # Restart primary
 "$INSTALL_DIR"/pg_ctl -D "$PRIMARY_DATADIR" -o "-p $PRIMARY_PORT" -l "$PRIMARY_LOGFILE" restart -m smart
 
 # Restart replica
 "$INSTALL_DIR"/pg_ctl -D "$REPLICA_DATADIR" -o "-p $REPLICA_PORT" -l "$REPLICA_LOGFILE" restart -m smart
 
-# fully set up replication
+# Set up replication
 TABLE_NAME="foo"
 TABLE2_NAME="bar"
 PUB="pub1"
 SUB="sub1"
+
+# Create table
 "${PSQL_PRIMARY[@]}" -c "CREATE TABLE ${TABLE_NAME}(a int, b int);"
 "${PSQL_PRIMARY[@]}" -c "CREATE TABLE ${TABLE2_NAME}(a int, b int);"
 "${PSQL_REPLICA[@]}" -c "CREATE TABLE ${TABLE_NAME}(a int, b int);"
 "${PSQL_REPLICA[@]}" -c "CREATE TABLE ${TABLE2_NAME}(a int, b int);"
+
 "${PSQL_PRIMARY[@]}" -c "CREATE PUBLICATION $PUB FOR TABlE $TABLE_NAME, $TABLE2_NAME;"
 "${PSQL_REPLICA[@]}" -c "CREATE SUBSCRIPTION $SUB CONNECTION 'dbname=$DB user=mplageman host=localhost port=$PRIMARY_PORT' PUBLICATION $PUB;"
+
 "${PSQL_PRIMARY[@]}" -c "INSERT INTO $TABLE_NAME SELECT i, i FROM generate_series(1,10)i;"
 "${PSQL_PRIMARY[@]}" -c "INSERT INTO $TABLE2_NAME SELECT 1, 2 FROM generate_series(1,3)i;"
